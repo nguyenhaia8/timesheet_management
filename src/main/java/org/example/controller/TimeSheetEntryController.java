28abae49d7eafa519d9217f4fe4fc1344e51d115
hainguyen <nguyenhaia8@gmail.com>
2025-07-14 21:28:23 -0500

    update cors

--- src/main/java/org/example/controller/TimeSheetEntryController.java
+++ src/main/java/org/example/controller/TimeSheetEntryController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/timesheet-entries")
-@CrossOrigin(origins = "*")
 public class TimeSheetEntryController {
 
     @Autowired
