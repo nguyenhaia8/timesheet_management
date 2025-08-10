df587e348c49ae4b2de15b18785e26d2ca2263b2
hainguyen <nguyenhaia8@gmail.com>
2025-08-10 10:49:26 -0500

    update new api timesheet

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig {
                 .requestMatchers("/h2-console/**").permitAll()
                 .requestMatchers("/api/employees").permitAll()
                 .requestMatchers("/api/departments").permitAll()
+                .requestMatchers("/api/projects").permitAll()
                 .anyRequest().authenticated()
             );
 
